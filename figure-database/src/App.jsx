b54b7b966fc7fbed6710f5b2be1f1cb8be8daa5a
Thanh <cringeclone@gmail.com>
2024-11-28 16:21:30 +0900

    first commit

---
+++ figure-database/src/App.jsx
@@ -0,0 +1,18 @@
+import { Fragment, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Fragment>
+      <div>
+        <h1>NOTHING</h1>
+      </div>
+    </Fragment>
+  )
+}
+
+export default App
